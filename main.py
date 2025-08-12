9aba58998864d44f13d9471bfe22e2586b024555
Itsmemonzu <mrmonzu77@gmail.com>
2025-08-12 18:28:51 +0600

    Enhance To Do List functionality with work completion and removal features

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import emoji
+import re
 
 works = []
 quit = False
@@ -18,9 +20,10 @@ while not quit:
             print("5. Exit")
             print("")
         start_menu()
-        #User Input
+
         userInput = input("= ")
-        
+
+        # Exit
         if userInput == "5":
             print("")
             print("Exiting the program.. Goodbye!")
@@ -29,7 +32,90 @@ while not quit:
         if userInput == "1":
             print("")
             work = input("Add the work: ")
-            works.append(work)
+            works.append("❌ "+work)
+            print("")
             print(f"'{work}' has been added to the list!")
+
+        if userInput == "2":
+
+            print("")
+            if not works:
+                print("No works to mark as completed.")
+            else:
+                number = 1
+
+                print("Select the work you want to mark as completed:")
+                for work in works:
+                    print(f"{number}. {work}") 
+                    number += 1
+                # Input for removing work
+                print("")
+                try:
+                    workDel = int(input("= "))-1
+                except:
+                    print("")
+                    print("You must input a correct number!")
+                
+                # Error: Work is already marked as completed
+                if not workDel > len(works)-1 and workDel >= 0 and "✅" in works[workDel]:
+                    print("")
+                    print("The work is already marked as completed!")
+                # Marking work as completed
+                elif not workDel > len(works)-1 and workDel >= 0 and not "✅" in works[workDel]:
+                    workDel = int(workDel)
+                    
+                    works[workDel] = re.sub(r'❌ ', '✅ ', works[workDel])
+                    print("")
+                    print(f"'{re.sub(r'✅ ', '', works[workDel])}' has been marked as complete! ✅")
+                else:
+                    print("")
+                    print("Wrong input! Please try again.")
+        if userInput == "3":
+
+            print("")
+            if not works:
+                print("No works to remove.")
+            else:
+                number = 1
+
+                print("Select the work you want to remove:")
+                for work in works:
+                    print(f"{number}. {work}") 
+                    number += 1
+                # Input for deleting work
+                print("")
+                try:
+                    workDel = int(input("= "))-1
+                except:
+                    print("")
+                    print("You must input a correct number!")
+                
+                if not workDel > len(works)-1 and workDel >= 0:
+                    workDel = int(workDel)
+                    if "❌" in works[workDel]:
+                        works[workDel] = re.sub(r'❌ ', '', works[workDel])
+                    elif "✅" in works[workDel]:
+                        works[workDel] = re.sub(r'✅ ', '', works[workDel])
+                    print("")
+                    print(f"'{works[workDel]}' has been removed! 🗑️")
+                    works.pop(workDel)
+                else:
+                    print("")
+                    print("Wrong input! Please try again.")
+        if userInput == "4":
+            if works:
+                number = 1
+                
+                print("")
+                print("List of works:")
+                for work in works:
+                    print(f"{number}. {work}") 
+                    number += 1
+            else:
+                print("")
+                print("There are no works to list!")
+
+
+                
 
         
